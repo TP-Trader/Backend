fff61060cd39d47246c433c3cb638784c534ac46
wesley.moody <wgm3190@gmail.com>
2020-03-30 19:37:27 -0500

    think backend is done need to test in postmanand do documentation

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authRouter = require("../auth/auth-router");
+const postsRouter = require('../posts/posts-router');
 const cors = require("cors");
 const helmet = require("helmet");
 
@@ -13,6 +14,7 @@ server.use(cors());
 server.use(helmet());
 
 server.use("/api/auth", authRouter);
+server.use('/api/posts', restricted, postsRouter);
 
 //  sanity check  >>>>>>>>
 server.get("/", (req, res) => {
